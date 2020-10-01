39d95fd1ee7dd565e6afa7ecf4a4fb1be5eb8fba
JeGuina <47313516+JeGuina@users.noreply.github.com>
2020-10-01 15:02:56 +0800

    modified clear command

--- commands/clear.js
+++ commands/clear.js
@@ -3,15 +3,28 @@ module.exports = {
     description: "Clear messages",
 
     async run (client, message, args){
-        const amount = args.join(" ");
-        if(!amount) return message.reply('Please provide an amount');
+        const messageArray = message.content.split(' ');
+        const args = messageArray.slice(1);
 
-        if(amount < 1 || amount > 100) return message.reply('Invalid amount');
+        if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`You are not allowed to use that command!`);
 
-        await message.channel.messages.fetch({limit: amount}).then(messages => {
-            message.channel.bulkDelete(messages);
-        })
+        let deleteAmount;
 
-        message.channel.send('Deleted ' +amount+ ' messages successfully!');
+        if(isNaN(args[0]) || parseInt(args[0] <= 0)){
+            return message.reply("Amount is not a number!");
+        }
+
+        if(parseInt(args[0]) < 1 || parseInt(args[0]) > 100){
+            return message.reply("Invalid amount!");
+        } else{
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount + 1, true);
+        message.channel.send(`Successfully deleted **${deleteAmount}** messages.`)
+        .then(msg => {
+            msg.delete({timeout: 10000})
+        });
+
     }
 }
